9d3594b1759db5c51b0a2c1d48e50bc4aeac9a4c
Thomas Yarnall <thomas.yarnall@experoinc.com>
2019-04-04 08:08:50 -0500

    Run prettier

--- src/modules/WellExplorer/components/CenterControl.js
+++ src/modules/WellExplorer/components/CenterControl.js
@@ -1,38 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { PropTypes } from 'prop-types';
-import L from 'leaflet';
-import { MapControl, withLeaflet } from 'react-leaflet';
+import React from "react";
+import ReactDOM from "react-dom";
+import { PropTypes } from "prop-types";
+import L from "leaflet";
+import { MapControl, withLeaflet } from "react-leaflet";
 
 class LegendControl extends MapControl {
+  static defaultProps = {
+    children: PropTypes.object
+  };
 
-    static defaultProps = { 
-        children: PropTypes.object
-    }
+  componentWillMount() {
+    const legend = L.control({ position: "bottomright" });
 
-    componentWillMount() {
-        const legend = L.control({ position: 'bottomright' });
+    //  TO-DO: Add legend here
+    const jsx = <div {...this.props}>{this.props.children}</div>;
 
-        //  TO-DO: Add legend here
-        const jsx = (
-            <div {...this.props}>
-                {this.props.children}
-            </div>
-        );
+    legend.onAdd = function(map) {
+      let div = L.DomUtil.create("div", "");
+      ReactDOM.render(jsx, div);
+      return div;
+    };
 
-        legend.onAdd = function (map) {
-            let div = L.DomUtil.create('div', '');
-            ReactDOM.render(jsx, div);
-            return div;
-        };
+    this.leafletElement = legend;
+  }
 
-        this.leafletElement = legend;
-    }
-
-    // Used by withLeaflet HOC to create element, otherwise component fails
-    createLeafletElement() {
-        return this.leafletElement
-    }
+  // Used by withLeaflet HOC to create element, otherwise component fails
+  createLeafletElement() {
+    return this.leafletElement;
+  }
 }
 
 export default withLeaflet(LegendControl);
--- src/modules/WellExplorer/components/WellMap.js
+++ src/modules/WellExplorer/components/WellMap.js
@@ -1,41 +1,33 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import CenterControl from './CenterControl';
+import React from "react";
+import PropTypes from "prop-types";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import CenterControl from "./CenterControl";
 
 const mapStyles = {
-    width: '100%',
-    minHeight: '60%',
-    maxHeight: '60%',
-    position: 'absolute',
-    left: 0,
-    margin: '0 auto',
-}
+  width: "100%",
+  minHeight: "60%",
+  maxHeight: "60%",
+  position: "absolute",
+  left: 0,
+  margin: "0 auto"
+};
 export const WellMap = ({ mapCenter, handleClickWell, markers, ...props }) => (
-    <Map
-        center={mapCenter}
-        length={4}
-        onClick={handleClickWell}
-        style={mapStyles}
-        zoom={6}
-        {...props}
-    >
-        <TileLayer
-            url='https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}'
-        />
-        {markers.length > 0 && markers.map((markerPosition, markerIndex) => (
-            <Marker key={markerIndex} position={markerPosition}>
-                <Popup>Info</Popup>
-            </Marker>
-        ))}
-        <CenterControl />
-    </Map>
-)
+  <Map center={mapCenter} length={4} onClick={handleClickWell} style={mapStyles} zoom={6} {...props}>
+    <TileLayer url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}" />
+    {markers.length > 0 &&
+      markers.map((markerPosition, markerIndex) => (
+        <Marker key={markerIndex} position={markerPosition}>
+          <Popup>Info</Popup>
+        </Marker>
+      ))}
+    <CenterControl />
+  </Map>
+);
 
 WellMap.propTypes = {
-    mapCenter: PropTypes.object.isRequired,
-    handleClickWell: PropTypes.func,
-    markers: PropTypes.arrayOf(PropTypes.array),
-}
+  mapCenter: PropTypes.object.isRequired,
+  handleClickWell: PropTypes.func,
+  markers: PropTypes.arrayOf(PropTypes.array)
+};
 
 export default WellMap;
