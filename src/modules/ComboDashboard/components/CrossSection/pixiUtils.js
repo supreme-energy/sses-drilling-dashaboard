b21d81926dd43597bdc0750bf5a618fd6e493606
Thomas Yarnall <thomas.yarnall@experoinc.com>
2019-04-16 12:04:16 -0500

    Add first version of custom transform

--- src/modules/ComboDashboard/components/CrossSection/pixiUtils.js
+++ src/modules/ComboDashboard/components/CrossSection/pixiUtils.js
@@ -137,4 +137,54 @@ function addDemoFormations(container, formations) {
   }
 }
 
-export { subscribeToMoveEvents, addGridlines, addDemoFormations };
+/**
+ * Transform function that does not affect an object's scale when transformed by
+ * the parent object.  Allows for an object to track position with other objects,
+ * but remain the same size.
+ * Updates the values of the object and applies the parent's transform.
+ *
+ * @param {PIXI.Transform} parentTransform - The transform of the parent of this object
+ */
+function customTransform(parentTransform) {
+  var lt = this.localTransform;
+
+  this._cx = Math.cos(this._rotation + this.skew._y);
+  this._sx = Math.sin(this._rotation + this.skew._y);
+  this._cy = -Math.sin(this._rotation - this.skew._x); // cos, added PI/2
+  this._sy = Math.cos(this._rotation - this.skew._x); // sin, added PI/2
+
+  if (this._localID !== this._currentLocalID) {
+    // get the matrix values of the displayobject based on its transform properties..
+    lt.a = this._cx * this.scale._x;
+    lt.b = this._sx * this.scale._x;
+    lt.c = this._cy * this.scale._y;
+    lt.d = this._sy * this.scale._y;
+
+    lt.tx = this.position._x - (this.pivot._x * lt.a + this.pivot._y * lt.c);
+    lt.ty = this.position._y - (this.pivot._x * lt.b + this.pivot._y * lt.d);
+    this._currentLocalID = this._localID;
+
+    // force an update..
+    this._parentID = -1;
+  }
+
+  if (this._parentID !== parentTransform._worldID) {
+    // concat the parent matrix with the objects transform.
+    var pt = parentTransform.worldTransform;
+    var wt = this.worldTransform;
+
+    wt.a = lt.a * pt.a + lt.b * pt.c;
+    wt.b = lt.a * pt.b + lt.b * pt.d;
+    wt.c = lt.c * pt.a + lt.d * pt.c;
+    wt.d = lt.c * pt.b + lt.d * pt.d;
+    wt.tx = lt.tx * pt.a + lt.ty * pt.c + pt.tx;
+    wt.ty = lt.tx * pt.b + lt.ty * pt.d + pt.ty;
+
+    this._parentID = parentTransform._worldID;
+
+    // update the id of the transform..
+    this._worldID++;
+  }
+}
+
+export { subscribeToMoveEvents, addGridlines, addDemoFormations, customTransform };
