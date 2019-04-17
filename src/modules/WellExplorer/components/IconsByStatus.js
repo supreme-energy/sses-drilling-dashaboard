08d2d1512de4abc5dc3a38785a6ec21163fadf14
Ioan Tomulesei <ioan.tomulesei@experoinc.com>
2019-04-17 15:40:54 +0300

    fixed typo

--- src/modules/WellExplorer/components/IconsByStatus.js
+++ src/modules/WellExplorer/components/IconsByStatus.js
@@ -1,9 +1,9 @@
 import { DRILLING, UNKNOWN, TRIPPING } from "../../../constants/drillingStatus";
 import drilling from "../assets/drilling-list.svg";
-import unkown from "../assets/unknown-list.svg";
+import unknown from "../assets/unknown-list.svg";
 import tripping from "../assets/tripping-list.svg";
 import drillingMap from "../assets/drilling-map.svg";
-import unkownMap from "../assets/unknown-map.svg";
+import unknownMap from "../assets/unknown-map.svg";
 import trippingMap from "../assets/tripping-map.svg";
 import drillingSelected from "../assets/Drilling-Selected.svg";
 import trippingSelected from "../assets/Tripping-Selected.svg";
@@ -11,13 +11,13 @@ import unknownSelected from "../assets/Unknown-Selected.svg";
 
 export const listIcons = {
   [DRILLING]: drilling,
-  [UNKNOWN]: unkown,
+  [UNKNOWN]: unknown,
   [TRIPPING]: tripping
 };
 
 export const mapIcons = {
   [DRILLING]: drillingMap,
-  [UNKNOWN]: unkownMap,
+  [UNKNOWN]: unknownMap,
   [TRIPPING]: trippingMap
 };
 
