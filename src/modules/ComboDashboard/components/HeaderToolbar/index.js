1c0a5837e5afc0b630ceda7fee41a3f23f6f6180
Oscar Hernandez <oscarhernandez@Oscars-MacBook-Pro.local>
2019-05-01 15:59:42 -0700

    Customize header toolbar with components and style

--- src/modules/ComboDashboard/components/HeaderToolbar/index.js
+++ src/modules/ComboDashboard/components/HeaderToolbar/index.js
@@ -1,6 +1,8 @@
-import React from "react";
-import { Card, CardContent, Typography, ListItem, List, ListItemText, ListItemIcon, Button } from "@material-ui/core";
+import React, { useState } from "react";
+import { Card, CardContent, CardHeader, IconButton, Icon, Collapse, Typography, ListItem, List, ListItemText, ListItemIcon, Button } from "@material-ui/core";
 import classes from "./HeaderToolbar.scss";
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
 import { BitDepth, Rop } from "../../../Kpi/KpiItem";
 import WellStatus from "../../../Kpi/WellStatus";
 import ServerStatus from "../../../Kpi/ServerStatus";
@@ -8,23 +10,30 @@ import WellPathStatus from "../../../Kpi/WellPathStatus";
 import classNames from "classnames";
 
 function HeaderToolbar({ theme, well, className }) {
-    console.log(well)
+    const [expanded, setExpanded] = useState(true);
     return (
-        <Card className={classNames(classes.card, className)}>
-            <CardContent>
+        <Card className={classes.card}>
+            <CardContent style={{ padding: 7, width: '100%' }}>
                 <div className={classes.cardContent}>
-                    <div>{well.name}</div>
-                    <span className={classes.hSpacer} />
                     <div className={classes.row}>
-                        <WellStatus status={well.status} className={classes.status} />
-                        <span className={classes.hBigSpacer} />
-                        <WellPathStatus wellId={well.id} />
-                        <span className={classes.hBigSpacer} />
-                        <Rop wellId={well.id} />
-                        <span className={classes.hBigSpacer} />
-                        <ServerStatus wellId={well.id} />
-                        <span className={classes.hBigSpacer} />
-                        <BitDepth wellId={well.id} />
+                        <Collapse in={expanded} unmountOnExit className={classes.collapseContainer} style={{ width: '100%' }}>
+                            <div className={classes.row}>
+                                <Typography variant="h5" color="primary">{well.name}</Typography>
+                                <span className={classes.hExtraLargeSpacer} />
+                                <WellStatus status={well.status} className={classes.status} />
+                                <span className={classes.hBigSpacer} />
+                                <WellPathStatus wellId={well.id} />
+                                <span className={classes.hBigSpacer} />
+                                <Rop wellId={well.id} />
+                                <span className={classes.hBigSpacer} />
+                                <ServerStatus wellId={well.id} />
+                                <span className={classes.hBigSpacer} />
+                                <BitDepth wellId={well.id} />
+                            </div>
+                        </Collapse>
+                        <IconButton className={classes.expandButton} onClick={() => setExpanded(!expanded)}>
+                            <MoreVertIcon />
+                        </IconButton>
                     </div>
                 </div>
             </CardContent>
