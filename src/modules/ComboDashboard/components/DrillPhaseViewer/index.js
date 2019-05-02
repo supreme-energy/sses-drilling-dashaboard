a525a4840dd1a5e76d0bfd00cbd9926098e46e88
Oscar Hernandez <oscarhernandez@Oscars-MacBook-Pro.local>
2019-05-02 14:16:30 -0700

    Clean up  code

--- src/modules/ComboDashboard/components/DrillPhaseViewer/index.js
+++ src/modules/ComboDashboard/components/DrillPhaseViewer/index.js
@@ -10,106 +10,85 @@ import DrillPhaseVerticalIcon from "./assets/drill-phase-vertical.svg";
 import classes from "./DrillPhaseViewer.scss";
 import classNames from "classnames";
 
-const gold = "#817618";
 const gray = "#757575";
 const green = "#09C501";
 
-const metaDataForPhaseViewer = {
+const iconForPhaseViewer = {
     [ON_SURFACE]: {
-        top: green,
-        curve: gray,
-        lateral: gray,
-        img: DrillPhaseVerticalIcon
+        icon: DrillPhaseVerticalIcon
     },
     [ON_INTERMEDIATE]: {
-        top: green,
-        curve: gray,
-        lateral: gray,
-        img: DrillPhaseVerticalIcon
+        icon: DrillPhaseVerticalIcon
 
     },
     [ON_DRILLOUT]: {
-        top: green,
-        curve: gray,
-        lateral: gray,
-        img: DrillPhaseVerticalIcon
+        icon: DrillPhaseVerticalIcon
     },
     [ON_CURVE]: {
-        top: gray,
-        curve: green,
-        lateral: gray,
-        img: DrillPhaseCurveIcon
+        icon: DrillPhaseCurveIcon
     },
     [ON_LATERAL]: {
-        top: gray,
-        curve: gray,
-        lateral: green,
-        img: DrillPhaseLateralIcon
-
+        icon: DrillPhaseLateralIcon
     }
 }
 
 function DrillPhaseViewer({ className }) {
     const [drillPhase, setDrillPhase] = useState(ON_SURFACE);
     const [anchorEl, setAnchorEl] = useState(null);
-    const drillPhaseEnum = Object.keys(metaDataForPhaseViewer);
+    const drillPhaseEnum = Object.keys(iconForPhaseViewer);
     const currStatus = drillPhaseEnum.includes(drillPhase) ? drillPhase : ON_SURFACE;
     const drillPhaseCode =  currStatus.split(" ")[1];
 
     return (
         <Card className={classNames(classes.card, className)}>
-            <CardContent style={{ padding: 0 }}>
-                <div className={classes.cardContent}>
-                    <ClickAwayListener onClickAway={() => setAnchorEl(null)}>
-                        <Button
-                            aria-owns={drillPhase ? 'simple-menu' : undefined}
-                            aria-haspopup="true"
-                            onClick={(e) => setAnchorEl(e.currentTarget)}
-                            className={classes.drillPhaseButton}
-                            style={{ borderRadius: 0, }}
-                        >
-                            <Typography style={{ color: gold }} variant="subtitle1">
-                                View
-                            </Typography>
-                            <img style={{marginBottom: 15 }} src={metaDataForPhaseViewer[currStatus].img} />
-                            <div>
-                                <div style={{ display: 'inline', marginLeft: 12 }}>{drillPhaseCode}</div>
-                                <ArrowDropDown />
-                            </div>
-                        </Button>
-                        <Menu
-                            id="simple-menu"
-                            anchorEl={anchorEl}
-                            open={Boolean(anchorEl)}
-                            onClose={() => setDrillPhase(currStatus)}
-                            disableAutoFocusItem
-                        >
-                            {drillPhaseEnum.map((phase, index) => {
-                                if (currStatus == phase) {
-                                    return (
-                                        <MenuItem
-                                            key={index}
-                                            className={classes.phaseMenuItem}
-                                            onClick={() => { setDrillPhase(phase); setAnchorEl(null); }}
-                                            style={{ minWidth: 185, backgroundColor: 'rgba(0, 0, 0, 0.15)' }}
-                                        >
-                                            <img src={metaDataForPhaseViewer[phase].img} />
-                                            <div style={{ marginLeft: 10 }}>{phase.split(" ")[1]}</div>
-                                            <CheckCircle style={{ position: 'absolute', right: 12, color: gray }} />
-                                        </MenuItem>
-                                    )
-                                } else {
-                                    return (
-                                        <MenuItem key={index} onClick={() => { setDrillPhase(phase); setAnchorEl(null); }}>
-                                            <img src={metaDataForPhaseViewer[phase].img} />
-                                            <div style={{ marginLeft: 10 }}>{phase.split(" ")[1]}</div>
-                                        </MenuItem>
-                                    )
-                                }
-                            })}
-                        </Menu>
-                    </ClickAwayListener>
-                </div>
+            <CardContent className={classes.cardContent}>
+                <ClickAwayListener onClickAway={() => setAnchorEl(null)}>
+                    <Button
+                        aria-owns={drillPhase ? 'simple-menu' : undefined}
+                        aria-haspopup="true"
+                        onClick={(e) => setAnchorEl(e.currentTarget)}
+                        className={classes.drillPhaseButton}
+                    >
+                        <Typography className={classes.drillPhaseButtonText} variant="subtitle1" gutterBottom>
+                            View
+                        </Typography>
+                        <img src={iconForPhaseViewer[currStatus].icon} />
+                        <div>
+                            <span className={classes.drillPhaseCode}>{drillPhaseCode}</span>
+                            <ArrowDropDown />
+                        </div>
+                    </Button>
+                    <Menu
+                        id="drill-phase-menu"
+                        anchorEl={anchorEl}
+                        open={Boolean(anchorEl)}
+                        onClose={() => setDrillPhase(currStatus)}
+                        disableAutoFocusItem
+                    >
+                        {drillPhaseEnum.map((phase, index) => {
+                            if (currStatus == phase) {
+                                return (
+                                    <MenuItem
+                                        key={index}
+                                        onClick={() => { setDrillPhase(phase); setAnchorEl(null); }}
+                                        style={{ minWidth: 185, backgroundColor: 'rgba(0, 0, 0, 0.15)' }}
+                                    >
+                                        <img src={iconForPhaseViewer[phase].icon} />
+                                        <div style={{ marginLeft: 10 }}>{phase.split(" ")[1]}</div>
+                                        <CheckCircle style={{ position: 'absolute', right: 12, color: gray }} />
+                                    </MenuItem>
+                                )
+                            } else {
+                                return (
+                                    <MenuItem key={index} onClick={() => { setDrillPhase(phase); setAnchorEl(null); }}>
+                                        <img src={iconForPhaseViewer[phase].icon} />
+                                        <div style={{ marginLeft: 10 }}>{phase.split(" ")[1]}</div>
+                                    </MenuItem>
+                                )
+                            }
+                        })}
+                    </Menu>
+                </ClickAwayListener>
             </CardContent>
         </Card>
     );
