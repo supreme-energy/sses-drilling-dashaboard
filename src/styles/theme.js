bcea4b772979c4bc1a780c5a9c409455a3a26436
ohernandez572 <oscar.hernandez@experoinc.com>
2019-05-03 17:28:10 -0700

    Add colors to palette for reference

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -22,7 +22,9 @@ export const theme = createMuiTheme({
     },
     warning: {
       main: "#EE2211"
-    }
+    },
+    gray: "#757575", // KPI color for reference
+    green: "#09C501" // KPI Color for reference
   },
   drillingStatusColor: "#4B921B",
   trippingStatusColor: "#B00020",
