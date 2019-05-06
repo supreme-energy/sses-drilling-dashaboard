2162105a568604f805268c22c3c674c5856040db
ohernandez572 <oscar.hernandez@experoinc.com>
2019-05-06 10:59:55 -0700

    Add more styling

--- src/modules/ComboDashboard/components/TimeSliderToolbar/index.js
+++ src/modules/ComboDashboard/components/TimeSliderToolbar/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Card, IconButton, Collapse } from "@material-ui/core";
-import ArrowDropDown from "@material-ui/icons/ArrowDropDown";
+import { ArrowDropDown, ArrowDropUp } from "@material-ui/icons";
 
 import DrillPhaseViewer from "./DrillPhaseViewer";
 import TimeSlider from "./TimeSlider";
@@ -9,17 +9,17 @@ import classes from "./TimeSliderToolbar.scss";
 function TimeSliderToolbar() {
   const [expanded, setExpanded] = useState(true);
   return (
-    <div style={{ display: "flex" }}>
+    <div style={{ display: "flex", minHeight: 50 }}>
       <Card className={classes.card}>
         <IconButton className={classes.collapseButton} onClick={() => setExpanded(e => !e)}>
-          <ArrowDropDown />
+          {expanded ? <ArrowDropDown /> : <ArrowDropUp />}
         </IconButton>
       </Card>
-      <Collapse in={expanded}>
-        <span style={{ display: "flex" }}>
+      <Collapse style={{ minHeight: 50, border: "1px solid rgb(209, 208, 208)" }} in={expanded}>
+        <div style={{ display: "flex" }}>
           <DrillPhaseViewer className={classes.noShrink} expanded={expanded} />
           <TimeSlider className={classes.noShrink} />
-        </span>
+        </div>
       </Collapse>
     </div>
   );
