e41bcd4a2925cb42fe5fd58b8bb013c5a9526451
Ethan Hasson <ethan.hasson@experoinc.com>
2019-05-13 10:35:17 -0500

    Added error state for file import.

--- src/store/files.js
+++ src/store/files.js
@@ -1,36 +1,59 @@
 export const ADD_FILE = "ADD_FILE";
+export const ADD_FILE_ERROR = "ADD_FILE_ERROR";
 
 export const addFile = (file) => async (dispatch) => {
   const reader = new FileReader();
-  const text = await new Promise((resolve, reject) => {
-    reader.onload = () => {
-      resolve(reader.result);
-    };
+  let text = null;
+  try {
+    text = await new Promise((resolve, reject) => {
+      reader.onload = () => {
+        resolve(reader.result);
+      };
 
-    reader.readAsText(file);
-  });
+      reader.onerror = (error) => {
+        reject(error);
+      };
 
-  dispatch({
-    type: ADD_FILE,
-    fileInfo: {
-      id: file.name,
-      file: file,
-      fileText: text,
+      reader.readAsText(file);
+    });
+
+    if (text) {
+      dispatch({
+        type: ADD_FILE,
+        fileInfo: {
+          id: file.name,
+          file: file,
+          fileText: text,
+        }
+      });
     }
-  });
+  } catch (error) {
+    dispatch({
+      type: ADD_FILE_ERROR,
+      error,
+    });
+  }
 };
 
 const HANDLERS = {
   [ ADD_FILE ]: (state, action) => {
     return {
       ...state,
+      error: null,
       files: [ action.file ],
+    };
+  },
+  [ ADD_FILE_ERROR ]: (state, action) => {
+    return {
+      ...state,
+      error: [ action.error ],
     };
   },
 };
 
 const initialState = {
-  files: []
+  files: [],
+  error: null
 };
 
 const reducer = (state = initialState, action) => {
