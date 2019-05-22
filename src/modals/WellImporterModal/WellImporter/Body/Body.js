18410f2582f1121bacbb5e5eae60f59f674b56a6
Ethan Hasson <ethan.hasson@experoinc.com>
2019-05-22 10:18:39 -0500

    DD-114: Remove extra code that is being used for next feature.

--- src/modals/WellImporterModal/WellImporter/Body/Body.js
+++ src/modals/WellImporterModal/WellImporter/Body/Body.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import AppAttributePaneBody from "../AppAttributePane/Body";
 import LASAttributePaneBody from "../LASAttributePane/Body";
 
 import css from "./styles.scss";
@@ -11,31 +10,11 @@ const Body = ({
                 className,
                 highlightedRowAndColumnList,
                 textHighlightedRowAndColumnList,
-                setHighlightedRowAndColumnList,
-                setTextHighlightedRowAndColumnList,
                 onClickCell,
-                appAttributesModel,
-                appAttributesFieldMapping,
-                activeInput,
-                activateInput,
-                combineHighlightedAndTextHighlightedMaps,
-                inputToCellId
               }) => {
   return (
     <div className={className}>
       <div className={css.appAttributePaneBodyContainer}>
-        <AppAttributePaneBody
-          activeInput={activeInput}
-          appAttributesModel={appAttributesModel}
-          appAttributesFieldMapping={appAttributesFieldMapping}
-          onFocus={(event, appAttributeConfig, appAttributeModel, fieldKey, sectionKey) => {
-            if (activeInput && activeInput.sectionKey === sectionKey && activeInput.fieldKey === fieldKey) {
-              activateInput(null);
-            } else {
-              activateInput({sectionKey, fieldKey});
-            }
-          }}
-        />
       </div>
       <div className={css.lasAttributePaneBodyContainer}>
         <LASAttributePaneBody
@@ -43,7 +22,6 @@ const Body = ({
           highlightedRowAndColumnList={highlightedRowAndColumnList}
           textHighlightedRowAndColumnList={textHighlightedRowAndColumnList}
           onClickCell={onClickCell}
-          unlocked={activeInput}
         />
       </div>
     </div>
@@ -58,14 +36,9 @@ Body.defaultProps = {
 Body.propTypes = {
   data: PropTypes.object.isRequired,
   onClickCell: PropTypes.func.isRequired,
-  appAttributesModel: PropTypes.object.isRequired,
-  appAttributesFieldMapping: PropTypes.object.isRequired,
-  setActiveInput: PropTypes.func.isRequired,
-  combineHighlightedAndTextHighlightedMaps: PropTypes.func.isRequired,
   className: PropTypes.string,
   highlightedRowAndColumnList: PropTypes.object,
   textHighlightedRowAndColumnList: PropTypes.object,
-  activeInput: PropTypes.string,
 };
 
 export default Body;
