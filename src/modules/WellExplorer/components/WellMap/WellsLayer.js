5ecb50ff7cdae8ab15096854f9cdc010f1afbe0a
Ioan Tomulesei <ioan.tomulesei@experoinc.com>
2019-05-29 10:49:50 +0300

    DD-82 update layout to support third column

--- src/modules/WellExplorer/components/WellMap/WellsLayer.js
+++ src/modules/WellExplorer/components/WellMap/WellsLayer.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { Marker } from "react-leaflet";
+import { mapIcons, mapIconsSelected } from "../IconsByStatus";
+import mapValues from "lodash/mapValues";
+import L from "leaflet";
+import classes from "./styles.scss";
 
-export default function WellsLayer() {
-  return null;
+const leafletIcons = mapValues(mapIcons, icon => L.icon({ iconUrl: icon }));
+const leafletIconsSelected = mapValues(mapIconsSelected, icon => L.icon({ iconUrl: icon }));
+
+export default function WellsLayer({ wells, onMarkerClick, selectedWellId }) {
+  return (
+    wells.length > 0 &&
+    wells.map(well => (
+      <Marker
+        key={well.id}
+        onClick={() => onMarkerClick(well)}
+        position={well.position}
+        icon={selectedWellId === well.id ? leafletIconsSelected[well.status] : leafletIcons[well.status]}
+        className={classes.marker}
+      />
+    ))
+  );
 }
