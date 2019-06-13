a827873aefd54876cc4f5fe36c2bfb0cf5e5b65d
Ioan Tomulesei <ioan.tomulesei@experoinc.com>
2019-06-13 13:40:16 +0300

    DD-148 small fixes

--- src/modules/WellExplorer/components/WellExplorer.js
+++ src/modules/WellExplorer/components/WellExplorer.js
@@ -1,36 +1,26 @@
-import React, { lazy, useState, useCallback } from 'react';
-import PropTypes from 'prop-types';
-import WellImporterModal from '../../../modals/WellImporterModal';
-import WellImporter from '../../../modals/WellImporterModal/WellImporter';
-import { addFile } from '../../../store/files';
-import classes from './WellExplorer.scss';
-import SearchCard from './SearchCard';
-import { withTheme } from '@material-ui/core/styles';
-import flowRight from 'lodash/flowRight';
-import { connect } from 'react-redux';
-import {
-  ALL_WELLS,
-  RECENT_WELLS,
-  FAVORITES,
-  changeActiveTab,
-  changeSelectedWell
-} from '../store';
-import { useWells, useWellsSearch } from '../../../api';
-import useMemo from 'react-powertools/hooks/useMemo';
-import WelcomeCard from './WelcomeCard';
-import memoizeOne from 'memoize-one';
-import WellOverview from './WellOverview';
-import classNames from 'classnames';
-import { getWellsZoomBounds, getWellZoomBounds } from '../utils/getWellsZoomBounds';
+import React, { lazy, useState, useCallback } from "react";
+import PropTypes from "prop-types";
+import WellImporterModal from "../../../modals/WellImporterModal";
+import WellImporter from "../../../modals/WellImporterModal/WellImporter";
+import { addFile } from "../../../store/files";
+import classes from "./WellExplorer.scss";
+import SearchCard from "./SearchCard";
+import { withTheme } from "@material-ui/core/styles";
+import flowRight from "lodash/flowRight";
+import { connect } from "react-redux";
+import { ALL_WELLS, RECENT_WELLS, FAVORITES, changeActiveTab, changeSelectedWell } from "../store";
+import { useWells, useWellsSearch } from "../../../api";
+import useMemo from "react-powertools/hooks/useMemo";
+import WelcomeCard from "./WelcomeCard";
+import memoizeOne from "memoize-one";
+import WellOverview from "./WellOverview";
+import classNames from "classnames";
+import { getWellsZoomBounds, getWellZoomBounds } from "../utils/getWellsZoomBounds";
 
-const WellMap = lazy(() =>
-  import(/* webpackChunkName: 'WellMap' */ './WellMap/index.js')
-);
+const WellMap = lazy(() => import(/* webpackChunkName: 'WellMap' */ "./WellMap/index.js"));
 
 const getRecentWells = memoizeOne((wells, wellTimestamps) => {
-  return wells
-    .filter(w => wellTimestamps[w.id])
-    .sort((a, b) => wellTimestamps[b.id] - wellTimestamps[a.id]);
+  return wells.filter(w => wellTimestamps[w.id]).sort((a, b) => wellTimestamps[b.id] - wellTimestamps[a.id]);
 });
 
 function getFilteredWells(activeTab, wells, wellTimestamps) {
@@ -59,12 +49,13 @@ export const WellExplorer = ({
 }) => {
   const [wells, wellsById, updateFavorite] = useWells();
 
-  const [searchTerm, onSearchTermChanged] = useState('');
+  const [searchTerm, onSearchTermChanged] = useState("");
   const [importModalShown, toggleShowImportModal] = useState(false);
-  const filteredWells = useMemo(
-    () => getFilteredWells(activeTab, wells, wellTimestamps),
-    [activeTab, wells, wellTimestamps]
-  );
+  const filteredWells = useMemo(() => getFilteredWells(activeTab, wells, wellTimestamps), [
+    activeTab,
+    wells,
+    wellTimestamps
+  ]);
 
   const recentWells = getRecentWells(wells, wellTimestamps);
   const mostRecentWell = recentWells[0];
@@ -74,9 +65,7 @@ export const WellExplorer = ({
   const selectedWell = wellsById[selectedWellId];
   const overviewMode = !!selectedWellId;
   const wellsBounds = useMemo(() => getWellsZoomBounds(wells), [wells]);
-  const selectedWellMapBounds = useMemo(() => getWellZoomBounds(selectedWell), [
-    selectedWell
-  ]);
+  const selectedWellMapBounds = useMemo(() => getWellZoomBounds(selectedWell), [selectedWell]);
   const onFilesToImportChange = event => {
     if (!event.target.files || !event.target.files.length) {
       return;
@@ -140,11 +129,7 @@ export const WellExplorer = ({
         </div>
         <span className={classes.hSpacer} />
         {selectedWellId ? (
-          <WellOverview
-            className={classes.wellOverview}
-            well={selectedWell}
-            updateFavorite={updateFavorite}
-          />
+          <WellOverview className={classes.wellOverview} well={selectedWell} updateFavorite={updateFavorite} />
         ) : (
           <WelcomeCard
             className={classes.welcome}
