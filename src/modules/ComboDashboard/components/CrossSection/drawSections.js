3414f13369ee23bae9c232e6840751125504ae27
Thomas Yarnall <thomas.yarnall@experoinc.com>
2019-07-10 10:17:40 -0500

    DD-190 Fix color of selected label

--- src/modules/ComboDashboard/components/CrossSection/drawSections.js
+++ src/modules/ComboDashboard/components/CrossSection/drawSections.js
@@ -36,7 +36,7 @@ function drawSections(container, higherContainer, props, gutter) {
   const labelBG = selectedLabel.addChild(new PIXI.Graphics());
   labelBG.position.x = -10;
   const memoInitLabel = memoizeOne(color => {
-    labelBG.beginFill(color, 1);
+    labelBG.clear().beginFill(color, 1);
     labelBG.drawRoundedRect(0, 0, 20, labelHeight, 5);
   });
 
@@ -94,7 +94,7 @@ function drawSections(container, higherContainer, props, gutter) {
       if (start + length < 0) continue;
       pixi.drawRoundedRect(start + 2, y, length - 4, buttonHeight, buttonHeight / 2);
 
-      if (selectedSections[p2.id]) {
+      if (isSelected) {
         selectedLeft.lineStyle(2, color[0], 0.5);
         selectedLeft.moveTo(start, 0).lineTo(start, height);
         selectedRight.lineStyle(2, color[0], 0.5);
