af6ffc05f562f43b9443564974de3b6d0584dd32
ohernandez572 <oscar.hernandez@experoinc.com>
2019-09-10 17:12:03 -0700

    DD-312: Connect fields to the backend

--- src/modules/WellExplorer/components/WellOverview/DataServer/ServerInfo.js
+++ src/modules/WellExplorer/components/WellOverview/DataServer/ServerInfo.js
@@ -3,39 +3,28 @@ import PropTypes from "prop-types";
 import TextField from "@material-ui/core/TextField";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
 import _ from "lodash";
 
 import { useSelectedWellInfoContainer } from "../../../../App/Containers";
 import Title from "../../../../../components/Title";
+import { fields, labels, connectionTypes, initialServerState } from "../../../../../constants/dataServer";
 import classes from "./styles.scss";
 
-const fields = {
-  USERNAME: "username",
-  PASSWORD: "password",
-  ENDPOINT: "host",
-  TYPE: "connection_type"
-};
-const connectionTypes = {
-  POLARIS: "polaris"
-};
-const initialServerState = {
-  [fields.USERNAME]: "",
-  [fields.PASSWORD]: "",
-  [fields.ENDPOINT]: "",
-  [fields.TYPE]: ""
-};
 function ServerInfo({ wellId }) {
   const [{ autorc }, , , refresh, , , , updateAutoRc] = useSelectedWellInfoContainer(wellId);
-
   const [values, setValues] = useState(initialServerState);
 
-  const differenceKey = useMemo(() => autorc && Object.keys(values).filter(k => values[k] !== autorc[k]), [
+  const differenceKey = useMemo(() => autorc && Object.keys(values).filter(k => values[k] !== autorc[k])[0], [
     values,
     autorc
   ]);
 
-  const handleInputChange = id => e => setValues(v => ({ ...v, [id]: e.target.value }));
-
+  const handleInputChange = id => e => {
+    const value = e.target.value;
+    setValues(v => ({ ...v, [id]: value }));
+  };
   const onFieldChange = useCallback(
     async (field, value, shouldRefreshStore) => {
       await updateAutoRc({ wellId, field, value });
@@ -51,7 +40,7 @@ function ServerInfo({ wellId }) {
       if (differenceKey && differenceKey.length) {
         e.returnValue = "Changes you made may not be saved.";
         if (!document.activeElement.id) {
-          onFieldChange(differenceKey, values[differenceKey[0]], true);
+          onFieldChange(differenceKey, values[differenceKey], true);
         }
       }
     },
@@ -67,45 +56,57 @@ function ServerInfo({ wellId }) {
     }
   }, [autorc]);
 
-  console.log(values, autorc);
-
   return (
-    <div className={classes.reportMailingListContainer}>
-      <Title>Report Mailing List</Title>
+    <div className={classes.serverInfoContainer}>
+      <Title>Server Info</Title>
       <form>
-        <Select
-          value={values[fields.TYPE]}
-          onChange={handleInputChange(fields.TYPE)}
-          inputProps={{
-            name: "type",
-            id: "server-type"
-          }}
-        >
-          {Object.keys(connectionTypes).map(type => (
-            <MenuItem value={type}>type</MenuItem>
-          ))}
-        </Select>
-        <TextField
-          label={fields.USERNAME}
-          value={values[fields.USERNAME]}
-          onChange={handleInputChange(fields.USERNAME)}
-          margin="normal"
-          onBlur={onBlur}
-        />
-        <TextField
-          label={fields.PASSWORD}
-          value={values[fields.PASSWORD]}
-          onChange={handleInputChange(fields.PASSWORD)}
-          margin="normal"
-          onBlur={onBlur}
-          type="password"
-        />
-        <TextField
-          label={fields.ENDPOINT}
-          value={values[fields.ENDPOINT]}
-          onChange={handleInputChange(fields.ENDPOINT)}
-          margin="normal"
-        />
+        <div className={classes.formContainer}>
+          <FormControl className={classes.textField}>
+            <InputLabel htmlFor="server-type">Server Type</InputLabel>
+            <Select
+              value={values[fields.TYPE]}
+              onChange={handleInputChange(fields.TYPE)}
+              onBlur={onBlur}
+              inputProps={{
+                name: "type",
+                id: "server-type"
+              }}
+            >
+              {_.map(connectionTypes, (value, key) => (
+                <MenuItem key={key} value={value}>
+                  {key}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <div className={classes.textInputContainer}>
+            <TextField
+              className={classes.textField}
+              label={labels.USERNAME}
+              value={values[fields.USERNAME]}
+              onChange={handleInputChange(fields.USERNAME)}
+              margin="normal"
+              onBlur={onBlur}
+            />
+            <TextField
+              className={classes.textField}
+              label={labels.PASSWORD}
+              value={values[fields.PASSWORD]}
+              onChange={handleInputChange(fields.PASSWORD)}
+              margin="normal"
+              onBlur={onBlur}
+              type="password"
+            />
+            <TextField
+              className={classes.endpointTextField}
+              label={labels.ENDPOINT}
+              value={values[fields.ENDPOINT]}
+              onChange={handleInputChange(fields.ENDPOINT)}
+              margin="normal"
+              onBlur={onBlur}
+            />
+          </div>
+        </div>
       </form>
     </div>
   );
