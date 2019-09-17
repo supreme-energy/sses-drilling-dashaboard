ff6e22b2958f3d3e8b4d589871f6e93887231473
Thomas Yarnall <thomas.yarnall@experoinc.com>
2019-09-17 09:39:13 -0500

    DD-322 Add import button over cross section

--- src/modules/ComboDashboard/components/CrossSectionDashboard.js
+++ src/modules/ComboDashboard/components/CrossSectionDashboard.js
@@ -6,6 +6,7 @@ import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import IconButton from "@material-ui/core/IconButton";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import Import from "@material-ui/icons/OpenInBrowser";
 import TableChartIcon from "../../../assets/tableChart.svg";
 
 import WidgetCard from "../../../components/WidgetCard";
@@ -18,10 +19,12 @@ import { HORIZONTAL, VERTICAL } from "../../../constants/crossSectionViewDirecti
 import { useCrossSectionContainer } from "../../App/Containers";
 import SelectedProjectionMethod from "./Details/SelectedProjectionMethod";
 import AutoPosTCLField from "./Details/AutoPosTCLField";
+import Button from "@material-ui/core/Button";
 
 export const CrossSectionDashboard = React.memo(({ wellId, className, view, updateView }) => {
   const [expanded, toggleExpanded] = useReducer(e => !e, false);
   const [showModal, toggleModal] = useReducer(m => !m, false);
+  const [showImportWellPlanModal, toggleImportWellPlanModal] = useReducer(m => !m, false);
   const [viewDirection, setViewDirection] = useState(0);
 
   const { selectedSections, calcSections } = useCrossSectionContainer();
@@ -55,6 +58,12 @@ export const CrossSectionDashboard = React.memo(({ wellId, className, view, upda
               />
             )}
           </ParentSize>
+          <div className={classes.importWellPlanButton}>
+            <Button onClick={toggleImportWellPlanModal}>
+              <Import />
+              Import Well Plan
+            </Button>
+          </div>
         </div>
         <div className={classes.cardLine} />
         <div className={classNames(classes.column, classes.shrink)}>
