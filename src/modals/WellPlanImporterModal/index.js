7b79d2275ba1a6e1c43af5b1b373bfd7e6a4c803
Thomas Yarnall <thomas.yarnall@experoinc.com>
2019-09-18 09:40:07 -0500

    DD-319 Add import modal

---
+++ src/modals/WellPlanImporterModal/index.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import {
+  Button,
+  IconButton,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  DialogContentText
+} from "@material-ui/core";
+
+import Close from "@material-ui/icons/Close";
+import classes from "./WellPlanImporter.scss";
+import { useWellPath, useWellPlanImport } from "../../api";
+import { useCrossSectionData, useWellIdContainer } from "../../modules/App/Containers";
+
+function WellPlanImporterModal({ handleClose, isVisible }) {
+  const { wellId } = useWellIdContainer();
+  const { uploadWellPlan } = useWellPlanImport();
+  const { refreshWellPlan } = useCrossSectionData();
+  const [file, setFile] = useState("");
+
+  const handleImport = async () => {
+    const data = new FormData();
+    data.append("userfile", file);
+
+    await uploadWellPlan(wellId, data);
+    console.log("refreshing");
+    refreshWellPlan();
+    console.log("refreshed");
+    handleClose();
+  };
+
+  const handleSelectFile = e => {
+    setFile(e.target.files[0]);
+  };
+  return (
+    <Dialog onClose={handleClose} maxWidth={false} aria-labelledby="customized-dialog-title" open={isVisible}>
+      <DialogTitle className={classes.dialogTitle}>
+        <span>Well Plan Importer</span>
+        <IconButton aria-label="Close" className={classes.closeButton} onClick={handleClose}>
+          <Close />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent className={classes.importDialogContent}>
+        <DialogContentText>Choose a file to import a new well plan</DialogContentText>
+        <input accept=".csv" id="manual-import-file" type="file" onChange={handleSelectFile} hidden />
+        <label htmlFor="manual-import-file">
+          <Button component="span" color="primary" variant="outlined">
+            Choose File
+          </Button>
+        </label>
+        <span className={classes.fileName}>{file.name}</span>
+      </DialogContent>
+      <DialogActions className={classes.importDialogActions}>
+        <Button color="primary" onClick={handleClose}>
+          Cancel
+        </Button>
+        <Button onClick={handleImport} disabled={!file.name} variant="contained" color="primary">
+          Import
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+WellPlanImporterModal.propTypes = {
+  handleClose: PropTypes.func,
+  isVisible: PropTypes.bool
+};
+
+export default WellPlanImporterModal;
